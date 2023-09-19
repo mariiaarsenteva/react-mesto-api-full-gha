4d39eb2e95620bb446cb8e643ee620c2d2a9b51b
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-09-19 10:16:14 +0300

    15пр

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,15 @@ const router = express.Router();
 
 const { PORT, MONGODB_URL } = process.env; // переменные прописаны в .env
 
-mongoose.connect(MONGODB_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+// mongoose.connect(MONGODB_URL, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// });
+
+mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: 'true',
+  useUnifiedTopology: 'true',
 });
 
 const app = express();
