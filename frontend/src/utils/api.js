d6ff99aee8dcfeeb23311d35d52f6bb21266675c
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-09-21 21:11:41 +0300

    15пр

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -103,10 +103,10 @@ class Api {
 //создаем экземпляр класса Api
 const api = new Api({
   baseUrl: baseUrl,
-  headers: {
+  // headers: {
     // authorization: `Bearer ${localStorage.getItem('jwt')}`,
-    'Content-Type': 'application/json'
-  }
+    // 'Content-Type': 'application/json'
+  // }
 });
 
 export default api
