9e2fba0036d925e91568a939c27ba9fea01d6b5d
Mariia Arsenteva <mariiaarsenteva@yandex.ru>
2023-09-21 19:27:40 +0300

    15пр

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -5,7 +5,7 @@ function getResData(res) {
 }
 
 export function registration(password, email) {
-  return fetch(`${baseUrl}/signup`, {
+  return fetch(`${baseUrl}signup`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -19,7 +19,7 @@ export function registration(password, email) {
 }
 
 export function authorization(password, email) {
-  return fetch(`${baseUrl}/signin`, {
+  return fetch(`${baseUrl}signin`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
